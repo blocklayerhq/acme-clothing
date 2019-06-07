ed856e49e5e708a7186101681bb77ff218091422
Andrea Luzzardi <aluzzardi@gmail.com>
2019-06-06 22:46:07 -0700

    Revert "test staging gh trigger"
    This reverts commit eef81bc424e1a3f92bf9d6ebc0fa95705988bdc6.

--- crate/code/web/src/modules/pages/Home.js
+++ crate/code/web/src/modules/pages/Home.js
@@ -41,7 +41,7 @@ const Home = (props) => (
         <H1 font="secondary" style={{ textShadow: textLevel1 }}>Crate</H1>
 
         <H4 style={{ textShadow: textLevel1, marginTop: '0.5em' }}>
-          STAGING Your monthly subscription of trendy clothes and accessories
+          Your monthly subscription of trendy clothes and accessories
         </H4>
 
         {/* Call to action */}
