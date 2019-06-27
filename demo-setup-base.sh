d396d7073c6074a001af38ffceb8bab46be323df
Andrea Luzzardi <aluzzardi@gmail.com>
2019-06-27 10:21:23 -0700

    prototype: attempt to reduce the amount of overrides required to spin up a microstaging
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- demo-setup-base.sh
+++ demo-setup-base.sh
@@ -3,5 +3,5 @@
 set -ex
 
 bl line rm acme-clothing-base || true
-bl line create acme-clothing-base -f ./acme-clothing.pipeline -d "Base Developer Pipeline for ACME"
+bl line create acme-clothing-base -f ./acme-clothing-base.pipeline -d "Base Developer Pipeline for ACME"
 ./blrun acme-clothing-base
--- demo-setup-staging.sh
+++ demo-setup-staging.sh
@@ -3,5 +3,5 @@
 set -ex
 
 bl line rm acme-clothing-staging || true
-bl line create acme-clothing-staging -f ./acme-clothing.pipeline -d "ACME Staging Pipeline"
+bl line create acme-clothing-staging -f ./acme-clothing-staging.pipeline -d "ACME Staging Pipeline"
 ./blrun acme-clothing-staging
